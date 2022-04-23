7491cbb923aac7749c4b79fe419418090a4f3e92
Bethsyf <bethsyfalcon@gmail.com>
2022-04-23 10:02:15 -0500

    funcionalidad listar, crear y eliminar

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,65 @@
+import React from 'react'
+import axios from 'axios';
+import { useState } from 'react';
+import { fileUpload } from '../helpers/fileUpload';
+import { url } from '../helpers/url';
 import '../styles/Form.css'
 
 const Form = () => {
 
+    const [vehiculos, setVehiculo] = useState({
+        modelo: '',
+        marca: '',
+        año: '',
+        placa: '',
+        imagen: ''
+    })
+      
+    const handleChanged = ({target}) => {
+        setVehiculo({
+          ...vehiculos,
+          [target.name]: target.value
+        })
+    
+      }
+
+      const handleSubmit = (e) => {
+       e.preventDefault();
+       axios.post(url, vehiculos)
+    .then(response => console.log(response))
+    .catch((error)=> console.log(error))
+      }
+
+      const handleFileChange = (e) => {
+        const file = e.target.files[0];
+         fileUpload(file)
+        .then(response => {
+            vehiculos.imagen = response;           
+        }).catch(error => {
+            console.log(error.message)
+        }) 
+    }
+
+
     return(
         <main>
-            <form class="form card">
+            <form class="form card" onSubmit={handleSubmit}>
                 <h2 class="form_heading">Crear Vehiculo</h2>
                 <hr/>
                 <label>Modelo
-                    <input class="input" type='text' name='modelo' id='modelo'/>
+                    <input class="input" type='text' name='modelo' id='modelo' onChange={handleChanged}/>
                 </label>
                 <label>Marca
-                    <input class="input" type='text' name='marca' id='marca'/>
+                    <input class="input" type='text' name='marca' id='marca' onChange={handleChanged}/>
                 </label>
                 <label>Año
-                    <input class="input" type='number' name='anio' id='anio'/>
+                    <input class="input" type='number' name='anio' id='anio' onChange={handleChanged}/>
                 </label>
                 <label>Placa
-                    <input class="input" type='text' name='placa' id='placa'/>
+                    <input class="input" type='text' name='placa' id='placa' onChange={handleChanged}/>
                 </label>
-                <label>Imagen
-                    <input class="input" type='file' name='imagen' id='btnImagen'/>
+                <label>Imagen 
+                    <input class="input" type="file" name='imagen' id='btnImagen' onChange={handleFileChange}  />
                 </label>
                 <button class='button' id='btnCrear'>Enviar</button>
             </form>            
--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { url } from '../helpers/url'
 import '../styles/List.css'
 
 const List = () => {
+    const [lista, setLista] = useState([])
+
+  const getData = ()=> {
+    axios.get(url)
+    .then(response => {
+      setLista(response.data);
+    }).catch(error=> console.log(error))
+  }
+
+  const deleteData = (id)=> {
+    axios.delete(`${url}/${id}`)
+    .then(response => {
+      alert("Vehiculo eliminado")
+      getData()
+    })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+  
+
 
     return(
         <main>
@@ -16,6 +40,20 @@ const List = () => {
                         <th>Accion</th>
                     </tr>
                 </thead>
+                <tbody>
+              {
+                lista.map(element =>(
+                  <tr key={element.id}>
+                    <td>{element.modelo}</td>
+                    <td>{element.marca}</td>
+                    <td>{element.año}</td>
+                    <td>{element.placa}</td>
+                    <td><img src={element.imagen} width="40" height="50" alt=""/></td>
+                    <td><button onClick={()=>deleteData(element.id)}>Eliminar</button></td>
+                  </tr>
+                ))
+              }
+            </tbody>
             </table>
         </main>
     )
